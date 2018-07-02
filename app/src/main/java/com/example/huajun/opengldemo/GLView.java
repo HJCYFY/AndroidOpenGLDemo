b613e503516857dedcdf2075c13773476d33d056
Hua Jun <hjcyfy@outlook.com>
2018-07-02 18:56:17 +0800

    opengl effect

--- app/src/main/java/com/example/huajun/opengldemo/GLView.java
+++ app/src/main/java/com/example/huajun/opengldemo/GLView.java
@@ -1,10 +1,16 @@
 package com.example.huajun.opengldemo;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.effect.Effect;
+import android.media.effect.EffectContext;
+import android.media.effect.EffectFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -102,22 +108,32 @@ public class GLView extends GLSurfaceView implements SurfaceHolder.Callback{
         private Square mSquare;
         private Triangle mTriangle;
         private float mScale = 1.f;
+        private int[] mTexName;
+
+        private EffectContext mEffectContext;
+        private Effect mEffect;
+        int mWidth,mHeight;
+
 
         public void onSurfaceCreated(GL10 gl, EGLConfig config){
             // 设置背景颜色
             GLES20.glClearColor(255,0,0,1.0f);
+
             mSquare = new Square();
             mTriangle = new Triangle();
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height){
             GLES20.glViewport(0,0,width,height);
+            mWidth = width;
+            mHeight = height;
             float ratio = (float)width/height;
             Matrix.frustumM(mProjectMatrix,0,-ratio,ratio,-1,1,3,7);
         }
 
         public void onDrawFrame(GL10 gl){
-            float[] scratch = new float[16];
+            getPicture();
+            effect();
             // 画背景颜色
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
             // 设置相机位置
@@ -125,13 +141,7 @@ public class GLView extends GLSurfaceView implements SurfaceHolder.Callback{
             // 计算投影及视图变化
             Matrix.multiplyMM(mMVPMatrix,0,mProjectMatrix,0,mViewMatrix,0);
             // 绘制正方形
-            mSquare.draw(mMVPMatrix);
-
-            Matrix.setRotateM(mRotateMatrix,0,mAngle,0,0,1.f);
-            Matrix.multiplyMM(scratch,0,mMVPMatrix,0,mRotateMatrix,0);
-            Matrix.scaleM(scratch,0,mScale,mScale,mScale);
-
-            mTriangle.draw(scratch);
+            mSquare.draw(mTexName[1]);
         }
 
         public static int loadShader(int type,String shaderCode) {
@@ -164,6 +174,33 @@ public class GLView extends GLSurfaceView implements SurfaceHolder.Callback{
         public void setScale(float scale) {
             mScale = scale;
             Log.d("HJ","scale "+scale);
+        }
+
+        public void getPicture() {
+            mTexName = new int[2];
+            GLES20.glGenTextures(2,mTexName,0);
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexName[0]);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+
+            Bitmap photo;
+            photo = BitmapFactory.decodeFile("/storage/emulated/0/DCIM/opengl.jpg");
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, photo, 0);
+        }
+
+        public void effect(){
+            if(mEffectContext == null) {
+                mEffectContext = EffectContext.createWithCurrentGlContext();
+            }
+            if(mEffect != null){
+                mEffect.release();
+            }
+            EffectFactory effectFactory = mEffectContext.getFactory();
+            mEffect = effectFactory.createEffect(EffectFactory.EFFECT_GRAYSCALE);
+            // 这里的宽高 应该设置成 GLView 的大小 以填充整个GLView
+            mEffect.apply(mTexName[0],mWidth,mHeight,mTexName[1]);
         }
     }
 
--- app/src/main/java/com/example/huajun/opengldemo/Square.java
+++ app/src/main/java/com/example/huajun/opengldemo/Square.java
@@ -20,38 +20,28 @@ import java.nio.ShortBuffer;
 
 public class Square {
     public final String vertexShaderCode =
-            // this matrix member variable provides a hook to manipulate
-            // the coordinates of the objects that use this vertex shader
             "attribute vec4 vPosition;" +
             "attribute vec2 aTexCoord;" +
             "varying vec2 vTexCoord;" +
-//            "uniform mat4 uMVPMatrix;" +
             "void main() {" +
-            // the matrix must be included as a modifier of gl_Position.
-            // Note that the uMVPMatrix factor *must be first* in order
-            // for the matrix multiplication product to be correct.
-            " gl_Position = vPosition;" +
-            " vTexCoord = aTexCoord;" +
+            "   gl_Position = vPosition;" +
+            "   vTexCoord = aTexCoord;" +
             "}";
 
     private final String fragmentShaderCode =
             "precision mediump float;" +
-            "uniform vec4 vColor;" +
             "varying vec2 vTexCoord;" +
             "uniform sampler2D sTexture;" +
             "void main() {" +
-            "gl_FragColor = vColor;" +
-            "gl_FragColor = texture2D(sTexture,vTexCoord);" +
+            "   gl_FragColor = texture2D(sTexture,vTexCoord);" +
             "}";
 
     private FloatBuffer vertexBuffer;
     private FloatBuffer textureBuffer;
     private int mProgram;
     private int mPositionHandle;
-    private int mColorHandle;
     private int mTexCoordHandle;
-    private int mTexSamplerHandler;
-    private int[] texName;
+    private int mTexSamplerHandle;
 
     // 每个顶点的坐标数量
     static final int COORDS_PER_VERTEX = 3;
@@ -64,26 +54,20 @@ public class Square {
 
     // 坐标系以屏幕左上角为原点
     private static float texCoords[] = {
-            1.f,0.f,
+            0.f,1.f,
             1.f,1.f,
             0.f,0.f,
-            0.f,1.f
+            1.f,0.f,
     };
 
     private final int vertexStride = COORDS_PER_VERTEX * 4; // COORDS_PER_VERTEX * sizeof(float)
 
-    float color[] = {0.f,0.f,1.f,0.0f};
-
     public Square(){
-        // initialize vertex byte buffer for shape coordinates
-        ByteBuffer bb = ByteBuffer.allocateDirect(
-                // (# of coordinate values * 4 bytes per float)
-                squareCoords.length * 4);
+        ByteBuffer bb = ByteBuffer.allocateDirect(squareCoords.length * 4);
         bb.order(ByteOrder.nativeOrder());
         vertexBuffer = bb.asFloatBuffer();
         vertexBuffer.put(squareCoords);
         vertexBuffer.position(0);
-
 
         ByteBuffer tb = ByteBuffer.allocateDirect(texCoords.length * 4);
         tb.order(ByteOrder.nativeOrder());
@@ -92,68 +76,40 @@ public class Square {
         textureBuffer.position(0);
 
         // prepare shaders and OpenGL program
-        int vertexShader = GLView.GLRender.loadShader(
-                GLES20.GL_VERTEX_SHADER,
-                vertexShaderCode);
-        int fragmentShader = GLView.GLRender.loadShader(
-                GLES20.GL_FRAGMENT_SHADER,
-                fragmentShaderCode);
+        int vertexShader = GLView.GLRender.loadShader( GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = GLView.GLRender.loadShader( GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
         mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
         GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
         GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
         GLES20.glLinkProgram(mProgram);
-
-
-        texName = new int[1];
-        GLES20.glGenTextures(1,texName,0);
-
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,texName[0]);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,GLES20.GL_TEXTURE_MIN_FILTER,GLES20.GL_LINEAR);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,GLES20.GL_TEXTURE_MAG_FILTER,GLES20.GL_LINEAR);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,GLES20.GL_TEXTURE_WRAP_S,GLES20.GL_REPEAT);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,GLES20.GL_REPEAT);
-
     }
 
-    public void draw(float[] mvpMatrix) {
-
+    public void draw(int texture) {
+        // 创建帧缓冲对象 FBO
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,0);
         // Add program to OpenGL environment
         GLES20.glUseProgram(mProgram);
-        GLES20.glDisable(GLES20.GL_BLEND);
+        // 渲染过程中禁止颜色混合
+//        GLES20.glDisable(GLES20.GL_BLEND);
 
         // get handle to vertex shader's vPosition member
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-
         // Enable a handle to the triangle vertices
         GLES20.glEnableVertexAttribArray(mPositionHandle);
-
         // Prepare the triangle coordinate data
-        GLES20.glVertexAttribPointer(
-                mPositionHandle, COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT, false,
-                vertexStride, vertexBuffer);
-
-        // get handle to fragment shader's vColor member
-        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
-
-        // Set color for drawing the triangle
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
-
-        // add Texture (预先在存储中放一张图像)
-        Bitmap bitmap = BitmapFactory.decodeFile("/storage/emulated/0/DCIM/opengl.jpg");
-        Log.d("HJ","w = "+bitmap.getWidth());
-        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D,0,bitmap,0);
-        bitmap.recycle();
+        GLES20.glVertexAttribPointer( mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
 
         mTexCoordHandle = GLES20.glGetAttribLocation(mProgram,"aTexCoord");
         GLES20.glEnableVertexAttribArray(mTexCoordHandle);
         GLES20.glVertexAttribPointer(mTexCoordHandle,2,GLES20.GL_FLOAT,false,8,textureBuffer);
 
-        mTexSamplerHandler = GLES20.glGetAttribLocation(mProgram,"sTexture");
-        GLES20.glUniform1i(mTexSamplerHandler,0);
 
+        mTexSamplerHandle = GLES20.glGetAttribLocation(mProgram,"sTexture");
+        GLES20.glUniform1i(mTexSamplerHandle,0);
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,texture);
         // Draw the square
         GLES20.glDrawArrays(
                 GLES20.GL_TRIANGLE_STRIP,0,4);
